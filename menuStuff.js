95c5928acee664b86f49f806efdc968f4077ccd0
abs <abs@ggl.pt>
2020-12-19 14:07:52 +0000

    login works

---
+++ menuStuff.js
@@ -0,0 +1,20 @@
+// Menu stuff
+
+function toggle(menuItem) {
+	switch (menuItem) {
+		case 'login':
+			$("#gantts").show();
+			$("#logout").show();
+			break;
+		case 'logout':
+		default:
+			$("#gantts").hide();
+			$("#logout").hide();
+			break;
+	}
+}
+
+function doLogout() {
+	firebase.auth().signOut();
+	toggle('logout');
+}
